4930d5d19b5aada6c90fc73bb8aef32b6af16848
jakkavignesh <81904688+jakkavignesh@users.noreply.github.com>
2024-05-14 09:14:16 +0530

    Update amazonServer.js

--- amazonServer.js
+++ amazonServer.js
@@ -166,7 +166,7 @@ app.post("/scrapeAmazon", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
 module.exports=app;
